cd47a2b588e0a07a6b690350f245a4f2239dd375
Aoran Xu <aaronlovelq@gmail.com>
2018-03-10 17:53:59 +0000

    Install bootstrap

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'rails', '4.2.6'
 # Use sqlite3 as the database for Active Record
 
 # Use SCSS for stylesheets
+gem 'bootstrap-sass', '~> 3.3.7'
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
@@ -36,15 +37,19 @@ group :development, :test do
   gem 'sqlite3'
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
-end
-
-group :development do
-  # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 end
+
+#group :development do
+  # Access an IRB console on exception pages or by using <%= console %> in views
+ # gem 'web-console', '~> 2.0'
+
+  # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
+  #gem 'spring'
+#end
 
 group :production do
   gem 'pg'
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:edit, :update, :show, :destroy]
+    
+    
     def index
        @articles = Article.all
     end
@@ -9,7 +12,7 @@ class ArticlesController < ApplicationController
     end
     
     def edit
-       @article = Article.find(params[:id]) 
+
     end
     
     def create
@@ -27,12 +30,12 @@ class ArticlesController < ApplicationController
     end
     
     def show
-       @article = Article.find(params[:id])
+
         
     end
     
     def destroy
-       @article = Article.find(params[:id]) 
+
        @article.destroy
        flash[:notice] = "Article was successfully deleted"
        redirect_to articles_path
@@ -40,7 +43,7 @@ class ArticlesController < ApplicationController
     end
     
     def update
-        @article = Article.find(params[:id])         
+      
         if @article.update(article_params)
             flash[:notice] = "Article was successfully updated"
             redirect_to article_path(@article)
@@ -50,6 +53,10 @@ class ArticlesController < ApplicationController
     end
     
     private
+        def set_article
+            @article = Article.find(params[:id])
+        end
+        
         def article_params
             params.require(:article).permit(:title, :description)       
         end
